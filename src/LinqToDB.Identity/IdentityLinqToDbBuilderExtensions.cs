655b30bd1cbd98c1764e7deb3b475031676fbe6d
Ilya Chudin <chudinia@gmail.com>
2017-07-06 13:07:34 +0500

    fix #5

--- src/LinqToDB.Identity/IdentityLinqToDbBuilderExtensions.cs
+++ src/LinqToDB.Identity/IdentityLinqToDbBuilderExtensions.cs
@@ -28,18 +28,13 @@ namespace Microsoft.Extensions.DependencyInjection
 		// ReSharper disable once InconsistentNaming
 		public static IdentityBuilder AddLinqToDBStores(this IdentityBuilder builder, IConnectionFactory factory)
 		{
-			builder.Services.AddSingleton(factory);
-
-			builder.Services.TryAdd(GetDefaultServices(
+			return AddLinqToDBStores(builder, factory,
 				typeof(string), 
-				builder.UserType, 
 				typeof(IdentityUserClaim<string>), 
 				typeof(IdentityUserRole<string>), 
 				typeof(IdentityUserLogin<string>), 
 				typeof(IdentityUserToken<string>), 
-				builder.RoleType, 
-				typeof(IdentityRoleClaim<string>)));
-			return builder;
+				typeof(IdentityRoleClaim<string>));
 		}
 
 		/// <summary>
@@ -55,17 +50,85 @@ namespace Microsoft.Extensions.DependencyInjection
 		public static IdentityBuilder AddLinqToDBStores<TKey>(this IdentityBuilder builder, IConnectionFactory factory)
 			where TKey : IEquatable<TKey>
 		{
-			builder.Services.AddSingleton(factory);
-
-			builder.Services.TryAdd(GetDefaultServices(
+			return AddLinqToDBStores(builder, factory,
 				typeof(TKey),
-				builder.UserType,
 				typeof(IdentityUserClaim<TKey>),
 				typeof(IdentityUserRole<TKey>),
 				typeof(IdentityUserLogin<TKey>),
 				typeof(IdentityUserToken<TKey>),
-				builder.RoleType,
-				typeof(IdentityRoleClaim<TKey>))); return builder;
+				typeof(IdentityRoleClaim<TKey>));
+		}
+
+		/// <summary>
+		///     Adds an linq2db implementation of identity information stores.
+		/// </summary>
+		/// <typeparam name="TKey">The type of the primary key used for the users and roles.</typeparam>
+		/// <typeparam name="TUserClaim">The type representing a claim.</typeparam>
+		/// <typeparam name="TUserRole">The type representing a user role.</typeparam>
+		/// <typeparam name="TUserLogin">The type representing a user external login.</typeparam>
+		/// <typeparam name="TUserToken">The type representing a user token.</typeparam>
+		/// <typeparam name="TRoleClaim">The type of the class representing a role claim.</typeparam>
+		/// <param name="builder">The <see cref="IdentityBuilder" /> instance this method extends.</param>
+		/// <param name="factory">
+		///     <see cref="IConnectionFactory" />
+		/// </param>
+		/// <returns>The <see cref="IdentityBuilder" /> instance this method extends.</returns>
+		// ReSharper disable once InconsistentNaming
+		public static IdentityBuilder AddLinqToDBStores<
+			TKey, 
+			TUserClaim, 
+			TUserRole, 
+			TUserLogin, 
+			TUserToken, 
+			TRoleClaim>(this IdentityBuilder builder, IConnectionFactory factory)
+			where TUserClaim : class, IIdentityUserClaim<TKey>
+			where TUserRole : class, IIdentityUserRole<TKey>
+			where TUserLogin : class, IIdentityUserLogin<TKey>
+			where TUserToken : class, IIdentityUserToken<TKey>
+			where TKey : IEquatable<TKey>
+			where TRoleClaim : class, IIdentityRoleClaim<TKey>
+		{
+
+			return AddLinqToDBStores(builder, factory,
+				typeof(TKey), 
+				typeof(TUserClaim), 
+				typeof(TUserRole), 
+				typeof(TUserLogin), 
+				typeof(TUserToken), 
+				typeof(TRoleClaim));
+		}
+
+		/// <summary>
+		///     Adds an linq2db implementation of identity information stores.
+		/// </summary>
+		/// <param name="builder">The <see cref="IdentityBuilder" /> instance this method extends.</param>
+		/// <param name="factory">
+		///     <see cref="IConnectionFactory" />
+		/// </param>
+		/// <param name="keyType">The type of the primary key used for the users and roles.</param>
+		/// <param name="userClaimType">The type representing a claim.</param>
+		/// <param name="userRoleType">The type representing a user role.</param>
+		/// <param name="userLoginType">The type representing a user external login.</param>
+		/// <param name="userTokenType">The type representing a user token.</param>
+		/// <param name="roleClaimType">The type of the class representing a role claim.</param>
+		/// <returns>The <see cref="IdentityBuilder" /> instance this method extends.</returns>
+		// ReSharper disable once InconsistentNaming
+		public static IdentityBuilder AddLinqToDBStores(this IdentityBuilder builder, IConnectionFactory factory,
+			Type keyType, Type userClaimType, Type userRoleType, Type userLoginType, Type userTokenType, Type roleClaimType)
+		{
+			builder.Services.AddSingleton(factory);
+
+			builder.Services.TryAdd(GetDefaultServices(
+				keyType, 
+				builder.UserType, 
+				userClaimType, 
+				userRoleType, 
+				userLoginType, 
+				userTokenType, 
+				builder.RoleType, 
+				roleClaimType));
+
+			return builder;
 		}
 
 		private static IServiceCollection GetDefaultServices(Type keyType, Type userType, Type userClaimType, Type userRoleType, Type userLoginType, Type userTokenType, Type roleType, Type roleClaimType)
---
+++ test/Microsoft.AspNetCore.Identity.EntityFrameworkCore.Test/ProvideConfigutayionTest.cs
@@ -0,0 +1,44 @@
+﻿using LinqToDB.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+// ReSharper disable InconsistentNaming
+
+namespace Microsoft.AspNetCore.Identity.EntityFrameworkCore.Test
+{
+	public class ProvideConfigutayionTest
+	{
+		[Fact]
+		public void AddLinqToDBStores0()
+		{
+			var services = new ServiceCollection();
+			services
+				.AddIdentity<IdentityUser, IdentityRole>()
+				.AddLinqToDBStores(new DefaultConnectionFactory());
+		}
+
+		[Fact]
+		public void AddLinqToDBStores1()
+		{
+			var services = new ServiceCollection();
+			services
+				.AddIdentity<IdentityUser<long>, IdentityRole<long>>()
+				.AddLinqToDBStores<long>(new DefaultConnectionFactory());
+		}
+
+		[Fact]
+		public void AddLinqToDBStores6()
+		{
+			var services = new ServiceCollection();
+			services
+				.AddIdentity<IdentityUser<decimal>, IdentityRole<decimal>>()
+				.AddLinqToDBStores<
+					decimal, 
+					IdentityUserClaim<decimal>, 
+					IdentityUserRole<decimal>, 
+					IdentityUserLogin<decimal>, 
+					IdentityUserToken<decimal>, 
+					IdentityRoleClaim<decimal>>(new DefaultConnectionFactory());
+		}
+
+	}
+}
