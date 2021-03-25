e02a9e0eeeadc13626c6e55ad4d76b0a3435ecfa
MaceWindu <MaceWindu@users.noreply.github.com>
2021-03-25 14:33:32 +0100

    v3.3.0

--- src/LinqToDB.Identity/Properties/AssemblyInfo.cs
+++ src/LinqToDB.Identity/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Resources;
 
 [assembly: AssemblyMetadata("Serviceable", "True")]
 [assembly: NeutralResourcesLanguage("en-us")]
-[assembly: AssemblyCompany("blog.linq2db.com")]
-[assembly: AssemblyCopyright("© 2011-2017 blog.linq2db.com")]
+[assembly: AssemblyCompany("linq2db.com")]
+[assembly: AssemblyCopyright("© 2011-2021 linq2db.com")]
 [assembly: AssemblyProduct("Linq to DB")]
